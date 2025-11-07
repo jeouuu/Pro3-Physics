f3cfbc43d66e4a9bef00cba99c90cb392e493764
JoeyLam <joeylam020402@gmail.com>
2025-11-07 15:48:56 -0500

    Journal 5 in Progress.

--- Assets/Scripts/Journal5/Player.cs
+++ Assets/Scripts/Journal5/Player.cs
@@ -3,20 +3,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;    
+    public float jumpForce= 5f; 
 
     private Rigidbody2D myBody;
     private SpriteRenderer mySprite;
     private float h;
+    private bool jump;
+    private bool onGround;
 
     void Start()
     {
         myBody = GetComponent<Rigidbody2D>();
-        mySprite = GetComponent<SpriteRenderer>();  
+        mySprite = GetComponent<SpriteRenderer>();
+
     }
 
     void Update()
     {
         h = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
 
         if (h < 0)
         {
@@ -30,12 +39,39 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         PlayerMovement(h);
+        if (jump && onGround)
+        {
+            PlayerJump();
+            jump = false;
+        }
+
     }
 
     private void PlayerMovement(float hori)
     {
-        Vector2 dir = new Vector2(hori, 0);
-        myBody.linearVelocity = dir * moveSpeed;
+        Vector2 dir = Vector2.right * hori;
+        myBody.linearVelocityX = dir.x * moveSpeed;
+    }
+
+    private void PlayerJump()
+    {
+        myBody.AddForce(Vector2.up * jumpForce , ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
     }
 
 
