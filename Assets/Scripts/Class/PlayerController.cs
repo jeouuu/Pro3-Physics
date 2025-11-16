f99da26cb3dc27283a1b3fad8c69ece5ee700741
JoeyLam <joeylam020402@gmail.com>
2025-11-15 21:37:30 -0500

    Journal 6 Task1: player horizontal movement. Done.

--- Assets/Scripts/Class/PlayerController.cs
+++ Assets/Scripts/Class/PlayerController.cs
@@ -7,8 +7,12 @@ public class PlayerController : MonoBehaviour
     public float accTime;
     public float decTime;
 
+    public bool use1 ;
+    public bool useUpdate ;
+
     private Vector2 moveInput;
     private Rigidbody2D playerRB;
+    private Vector2 vel = Vector2.zero;
 
 
     public enum FacingDirection
@@ -24,32 +28,84 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         GetPlayerInput();
+
+        if (useUpdate)
+        {
+            MoveWithTransform();
+        }
     }
 
     private void FixedUpdate()
     {
-        MovementUpdate(moveInput);
+        if (!useUpdate)
+        {
+            MovementUpdate(moveInput);
+
+        }
+    }
+
+    #region Player Movement
+    private void MoveWithTransform()
+    {
+        float acc = moveSpeed / accTime;
+        float dec = moveSpeed / decTime;
+        if (moveInput.x != 0)
+        {
+            vel += acc * Time.deltaTime * new Vector2(moveInput.x, 0);
+        } else if (moveInput.x == 0)
+        {
+            vel -= dec * Time.deltaTime * vel.normalized;
+        }
+
+        vel = Vector3.ClampMagnitude(vel, moveSpeed);
+        transform.position += (Vector3)vel * Time.deltaTime;
     }
 
     private void MovementUpdate(Vector2 _moveInput)
     {
-        float targetSpeed = _moveInput.x * moveSpeed;
-        float acc = moveSpeed / accTime;
-        float dec = moveSpeed / decTime;
+        // method 1
+        if (use1)
+        {
+            float targetSpeed = _moveInput.x * moveSpeed;
+            float acc = moveSpeed / accTime;
+            float dec = moveSpeed / decTime;
 
-        float accRate = 0; ;
-        if(_moveInput.x != 0)
-        {
-            accRate = acc;
-        }else if(_moveInput.x == 0)
-        {
-            accRate = dec;
+            float accRate = 0; ;
+            if (_moveInput.x != 0)
+            {
+                accRate = acc;
+            } else if (_moveInput.x == 0)
+            {
+                accRate = dec;
+            }
+
+            float speedDiff = targetSpeed - playerRB.linearVelocityX;
+
+            Vector2 force = new Vector2(speedDiff * accRate * playerRB.mass, 0f);
+            playerRB.AddForce(force);
         }
 
-        float speedDiff = targetSpeed - playerRB.linearVelocityX;
-        Vector2 force = new Vector2( speedDiff * accRate * playerRB.mass , 0f);
-        playerRB.AddForce(force);
+
+        //method 2
+        else if (!use1)
+        {
+            Vector2 vel = playerRB.linearVelocity;
+            float acc = moveSpeed / accTime;
+            float dec = moveSpeed / decTime;
+
+            if (_moveInput.x != 0)
+            {
+                vel += acc * Time.fixedDeltaTime * new Vector2(_moveInput.x, 0);
+            } else if (_moveInput.x == 0)
+            {
+                vel -= dec * Time.fixedDeltaTime * vel.normalized;
+            }
+
+            vel = Vector2.ClampMagnitude(vel, moveSpeed);
+            playerRB.linearVelocity = vel;
+        }
     }
+    #endregion
 
     private void GetPlayerInput()
     {
